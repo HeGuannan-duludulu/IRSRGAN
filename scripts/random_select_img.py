1fab8006a7b50b9656c57a04b74a51b245d5bb1c
Administrator <guannan202211@163.com>
2023-02-05 00:41:50 +0800

    initial commit

--- scripts/random_select_img.py
+++ scripts/random_select_img.py
@@ -1,13 +1,23 @@
 
 import os
 import random
+import shutil
 
-all_img_dir = r'../data/all_img'
+all_img_dir = r'..\data\all_img'
 
 all_pic_name = os.listdir(all_img_dir)
-print(len(all_pic_name))
-a = random.sample(all_pic_name, 15000)
 
-output_dir = r'../data/CVC-09_14(train)/original'
-remain_img_dir = r'../data/CVC-09_14(test)'
+random_selected = random.sample(all_pic_name, 15000)
+not_selected = [img for img in all_pic_name if img not in random_selected]
 
+
+output_dir = r'..\data\CVC-09_14(train)\original'
+not_selected_img_dir = r'..\data\CVC-09_14(test)'
+
+for img_name in random_selected:
+    shutil.copy(os.path.join(all_img_dir, img_name), os.path.join(output_dir, img_name))
+
+for img_name in not_selected:
+    shutil.copy(os.path.join(all_img_dir, img_name), os.path.join(not_selected_img_dir, img_name))
+
+print(os.path.join(all_img_dir, random_selected[0]))
